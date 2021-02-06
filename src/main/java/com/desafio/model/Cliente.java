8abf7e57e3bacb8ba65c7549c2c88e23098d9bf6
Raul Castro <raulc27@gmail.com>
2021-02-06 15:20:25 -0300

    alterando Cliente.java

--- src/main/java/com/desafio/model/Cliente.java
+++ src/main/java/com/desafio/model/Cliente.java
@@ -2,9 +2,52 @@ package com.desafio.model;
 
 public class Cliente {
 	
-	String nome;
-	String login;
-	String cpf;
-	String senha;
 
+	private String nome;
+	private String login;
+	private String cpf;
+	private String senha;
+
+	
+	public Cliente() {
+		
+	}
+	
+	public Cliente(int id, String login, String nome, String cpf, String senha) {
+		super();
+		this.login = login;
+		this.senha = senha;
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public String getCPF() {
+		return cpf;
+	}
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public void setCPF(String cpf) {
+		this.cpf = cpf;
+	}
+
+	
+	
+	
 }
--- src/main/java/com/desafio/model/PlanoContas.java
+++ src/main/java/com/desafio/model/PlanoContas.java
@@ -2,4 +2,10 @@ package com.desafio.model;
 
 public class PlanoContas {
 
+	Double valor;
+	Integer id_usuario;
+	String descricao;
+	
+	
+	
 }
