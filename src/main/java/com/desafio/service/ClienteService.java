92e4695952f929843eaeb87aca47c6a910dabb91
Raul Castro <raulc27@gmail.com>
2021-02-06 17:59:14 -0300

    criando service

--- src/main/java/com/desafio/service/ClienteService.java
+++ src/main/java/com/desafio/service/ClienteService.java
@@ -1,5 +1,15 @@
 package com.desafio.service;
+import com.desafio.model.Cliente;
 
 public class ClienteService {
-
+public Cliente criarCliente(String login) {
+	if(login==null)
+		throw new NullPointerException("O campo login é obrigatório");
+	if(login.length()>20)
+		throw new IllegalArgumentException("O campo permite até 20 caracteres");
+	
+	Cliente user = new Cliente();
+	user.setLogin(login);
+	return user;
+}
 }
